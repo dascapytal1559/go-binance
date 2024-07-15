21932dc10dd4872cba98b46734d9adf498a9ed11
Das Capytal <dascapytal1559@gmail.com>
2024-07-15 17:24:07 +1000

    queryUMOrder

--- v2/pmargin/um_query_order_service.go
+++ v2/pmargin/um_query_order_service.go
@@ -8,44 +8,8 @@ import (
 	"github.com/adshao/go-binance/v2/futures"
 )
 
-// ListUMOpenOrdersService list opened orders
-type ListUMOpenOrdersService struct {
-	c      *Client
-	symbol *string
-}
-
-// Symbol set symbol
-func (s *ListUMOpenOrdersService) Symbol(symbol string) *ListUMOpenOrdersService {
-	s.symbol = &symbol
-	return s
-}
-
-// Do send request
-func (s *ListUMOpenOrdersService) Do(ctx context.Context, opts ...RequestOption) (res []*UMOpenOrder, err error) {
-	r := &request{
-		method:   http.MethodGet,
-		endpoint: "/papi/v1/um/openOrders",
-		secType:  secTypeSigned,
-	}
-
-	r.setParams(params{
-		"symbol": s.symbol,
-	})
-
-	data, _, err := s.c.callAPI(ctx, r, opts...)
-	if err != nil {
-		return []*UMOpenOrder{}, err
-	}
-	res = make([]*UMOpenOrder, 0)
-	err = json.Unmarshal(data, &res)
-	if err != nil {
-		return []*UMOpenOrder{}, err
-	}
-	return res, nil
-}
-
 // OrderResponse define order info
-type UMOpenOrder struct {
+type UMQueryOrderResponse struct {
 	AvgPrice                string                   `json:"avgPrice"`
 	ClientOrderId           string                   `json:"clientOrderId"`
 	CumQuote                string                   `json:"cumQuote"`
@@ -65,4 +29,91 @@ type UMOpenOrder struct {
 	UpdateTime              int64                    `json:"updateTime"`
 	SelfTradePreventionMode string                   `json:"selfTradePreventionMode"`
 	GoodTillDate            int64                    `json:"goodTillDate"`
+}
+
+// QueryUMOrder list opened orders
+type QueryUMOrder struct {
+	c                 *Client
+	symbol            string
+	orderId           *int64
+	origClientOrderId *string
+}
+
+// Symbol set symbol
+func (s *QueryUMOrder) Symbol(symbol string) *QueryUMOrder {
+	s.symbol = symbol
+	return s
+}
+
+// OrderID set orderID
+func (s *QueryUMOrder) OrderID(orderId int64) *QueryUMOrder {
+	s.orderId = &orderId
+	return s
+}
+
+// OrigClientOrderID set origClientOrderID
+func (s *QueryUMOrder) OrigClientOrderID(origClientOrderId string) *QueryUMOrder {
+	s.origClientOrderId = &origClientOrderId
+	return s
+}
+
+// Do send request
+func (s *QueryUMOrder) Do(ctx context.Context, opts ...RequestOption) (res *UMQueryOrderResponse, err error) {
+	r := &request{
+		method:   http.MethodGet,
+		endpoint: "/papi/v1/um/order",
+		secType:  secTypeSigned,
+	}
+
+	r.setParams(params{
+		"symbol":            s.symbol,
+		"orderId":           s.orderId,
+		"origClientOrderId": s.origClientOrderId,
+	})
+
+	data, _, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(data, res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+// ListUMOpenOrdersService list opened orders
+type ListUMOpenOrdersService struct {
+	c      *Client
+	symbol *string
+}
+
+// Symbol set symbol
+func (s *ListUMOpenOrdersService) Symbol(symbol string) *ListUMOpenOrdersService {
+	s.symbol = &symbol
+	return s
+}
+
+// Do send request
+func (s *ListUMOpenOrdersService) Do(ctx context.Context, opts ...RequestOption) (res []*UMQueryOrderResponse, err error) {
+	r := &request{
+		method:   http.MethodGet,
+		endpoint: "/papi/v1/um/openOrders",
+		secType:  secTypeSigned,
+	}
+
+	r.setParams(params{
+		"symbol": s.symbol,
+	})
+
+	data, _, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return []*UMQueryOrderResponse{}, err
+	}
+	res = make([]*UMQueryOrderResponse, 0)
+	err = json.Unmarshal(data, &res)
+	if err != nil {
+		return []*UMQueryOrderResponse{}, err
+	}
+	return res, nil
 }
