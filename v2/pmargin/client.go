0d5405280594621d5eb952bf04c8a71cb439b8e6
Das Capytal <dascapytal1559@gmail.com>
2024-07-15 14:17:08 +1000

    added services

--- v2/pmargin/client.go
+++ v2/pmargin/client.go
@@ -247,6 +247,16 @@ func (c *Client) NewGetUMAccountService() *GetUMAccountService {
 	return &GetUMAccountService{c: c}
 }
 
+// NewCreateUMOrderService init creating UM order service
+func (c *Client) NewCreateUMOrderService() *CreateUMOrderService {
+	return &CreateUMOrderService{c: c}
+}
+
+// NewCancelUMOrderService init canceling UM order service
+func (c *Client) NewCancelUMOrderService() *CancelUMOrderService {
+	return &CancelUMOrderService{c: c}
+}
+
 // NewListUMOpenOrdersService init list UM open orders service
 func (c *Client) NewListUMOpenOrdersService() *ListUMOpenOrdersService {
 	return &ListUMOpenOrdersService{c: c}
